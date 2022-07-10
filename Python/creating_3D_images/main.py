d71bbfb0951c2b1f47eb0f9e74b2e30d432bb167
Miguel Vitor Macedo Dantas <miguelvitordantas@gmail.com>
2022-07-10 14:01:35 +0200

    primeiros arquivos

---
+++ Python/creating_3D_images/main.py
@@ -0,0 +1,8 @@
+import numpy
+import matplotlib
+import skimage
+import PyQt5
+
+
+if __name__ == "__main__":
+    img = './images/chair.jpeg'
---
+++ Python/creating_3D_images/reconizing_letters/functions.py
@@ -0,0 +1,5 @@
+import numpy
+import matplotlib
+import skimage
+
+
