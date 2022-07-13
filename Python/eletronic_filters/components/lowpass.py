3c33dba373554768d4cc76949193329607e02d41
Miguel Vitor Macedo Dantas <miguelvitordantas@gmail.com>
2022-07-13 20:16:31 +0200

    changing lowpass filter and creating lowpass2

--- Python/eletronic_filters/components/lowpass.py
+++ Python/eletronic_filters/components/lowpass.py
@@ -1,5 +1,5 @@
-from .components_eletronics import Resistor, Capacitor, Inductor
-from math import pi
+from components.components_eletronics import Resistor, Capacitor, Inductor
+from math import pi, sqrt
 
 
 class LowPassOrderOne:
@@ -10,7 +10,7 @@ class LowPassOrderOne:
         self.__cutoff_frequency: float = self.__calculate_cutoff_frequency(res1, cap1)
 
     @staticmethod
-    def __calculate_cutoff_frequency(resistance, capacitance):
+    def __calculate_cutoff_frequency(resistance: float, capacitance: float):
         return 1 / (2 * pi * resistance * capacitance)
 
     @property
@@ -33,9 +33,31 @@ class LowPassOrderTwo:
         self.__resistor2 = Resistor(res2)
         self.__capacitor1 = Capacitor(cap1)
         self.__capacitor2 = Capacitor(cap2)
+        self.__cutoff_frequency = self.__calculate_cutoff_frequency(res1, cap1, res2, cap2)
+
+    @property
+    def resistor1(self):
+        return self.__resistor1
+
+    @property
+    def capacitor1(self):
+        return self.__capacitor1
+
+    @property
+    def resistor2(self):
+        return self.__resistor2
+
+    @property
+    def capacitor2(self):
+        return self.__capacitor2
+
+    @staticmethod
+    def __calculate_cutoff_frequency(res1: float, cap1: float, res2: float, cap2: float):
+        return 1/(2 * pi * sqrt(res1 * res2 * cap1 * cap2))
 
 
 if __name__ == "__main__":
-    lp = LowPassOrderOne(1E3, 10E-6)
-    print(lp.resistor.resistance)
-    print(lp.capacitor.capacitance)
+    lp = LowPassOrderTwo(1E3, 10E-6)
+    print(lp.resistor1.resistance)
+    print(lp.capacitor1.capacitance)
+    print()
