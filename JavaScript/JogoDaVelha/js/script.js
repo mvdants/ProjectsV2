5ddc873cd74c7572fc173c0a5f0f898b2ad0ddb3
Miguel Vitor Macedo Dantas <miguelvitordantas@gmail.com>
2022-07-19 21:43:55 +0200

    cleaning the code

--- JavaScript/JogoDaVelha/js/script.js
+++ JavaScript/JogoDaVelha/js/script.js
@@ -9,38 +9,26 @@ let btn = document.querySelector(".btn");
 const imgs_x = document.querySelectorAll(".x");
 const imgs_o = document.querySelectorAll(".o"); 
 
+function create_list_of_elements(list_of_elements){
+    let lista = [];
+    for(let element of list_of_elements){
+        lista.push(make_mira_element(element));
+    }
+    return lista;
+}
+
 // Creating a list of elements of images X and O
-let list_x_elements = [];
-let list_o_elements = [];
+let list_x_elements = create_list_of_elements(imgs_x);
+let list_o_elements = create_list_of_elements(imgs_o);
 
-// Initializing the list of imgs that was clicked
-let list_imgx_clicked = [false, false, false, false, false];
-let list_imgo_clicked = [false, false, false, false, false];
+// Attribuate to the button function click -> set initial positions
+btn.onclick = () => {set_initial_positions(list_x_elements, list_o_elements);}
 
-// Inserting in the list the elements of x
-for(let element of imgs_x){
-    list_x_elements.push(element);
-}
-
-// Inserting in the list the elements of o
-for(let element of imgs_o){
-    list_o_elements.push(element);
-}
-
-// Getting initial values
-const initial_top_position = [50, 175, 300, 425, 550];
-const initial_x_left = "10%";
-const initial_o_left = "80%";
-
-btn.onclick = function(){  
-    for(let element of list_x_elements){
-        element.style.top = `${initial_top_position[list_x_elements.indexOf(element)]}px`;
-        element.style.left = initial_x_left;
-    }
-    for(let element of list_o_elements){
-        element.style.top = `${initial_top_position[list_o_elements.indexOf(element)]}px`;
-        element.style.left = initial_o_left;  
-    }
+function set_initial_positions(...list_of_element){
+    list_of_element.forEach((element) => {
+        element.element.style.top = `${element.init_top_pos}px`;
+        element.element.style.left = `${element.init_left_pos}px`;
+    });
 }
 
 for(let index in list_x_elements){
@@ -76,48 +64,50 @@ for(let index in list_x_elements){
 
 // Ading the mousemove action to the page
 document.addEventListener("mousemove", function(event){
+    // console.log(event.clientX, event.clientY);
     for(let index in list_x_elements){
 
         // Verifing if the img x was clicked before
         if(list_imgx_clicked[index] === true){
-            list_x_elements[index].style.top = event.clientY + "px";
-            list_x_elements[index].style.left = event.clientX + "px";
+            list_x_elements[index].style.top = event.clientY - offsetY + "px";
+            list_x_elements[index].style.left = event.clientX - offsetX + "px";
 
         // Verifing if the img o was clicked before
         }else if(list_imgo_clicked[index] === true){
-            list_o_elements[index].style.top = event.clientY + "px";
-            list_o_elements[index].style.left = event.clientX + "px";
+            list_o_elements[index].style.top = event.clientY - offsetY + "px";
+            list_o_elements[index].style.left = event.clientX - offsetX + "px";
         }
     }
 });
 
 function verify_position_left(position_left){
+    console.log(`pos_left: ${position_left}`);
     let pos_left = 0;
-    if(position_left > 300 && position_left < 500){
-        pos_left = 350;
-    }else if(position_left > 500 && position_left < 700){
-        pos_left = 550;
-    }else if(position_left > 700 && position_left < 900){
-        pos_left = 750;
+    if(position_left > 375 && position_left < 575){
+        pos_left = 475;
+    }else if(position_left > 575 && position_left < 775){
+        pos_left = 675;
+    }else if(position_left > 775 && position_left < 975){
+        pos_left = 875;
     }else{
         pos_left = position_left;
     }
-    console.log(`pos_left: ${pos_left}`);
     return pos_left;
 }
 
 function verify_position_top(position_top){
+    console.log(`pos_top: ${position_top}`);
     let pos_top = 0;
-    if(position_top > 100 && position_top < 300){
-        pos_top = 150;
-    }else if(position_top > 300 && position_top < 500){
-        pos_top = 350;
-    }else if(position_top > 500 && position_top < 700){
-        pos_top = 550;
+    if(position_top > 100 && position_top < 225){
+        pos_top = 125;
+    }else if(position_top > 225 && position_top < 400){
+        pos_top = 325;
+    }else if(position_top > 400 && position_top < 550){
+        pos_top = 525;
     }else{
         pos_top = position_top;
     }
-    console.log(`pos_top: ${pos_top}`);
+    
     return pos_top;
 }
 
@@ -136,7 +126,9 @@ function make_mira_element(element){
         element : element,
         init_left_pos: element.getBoundingClientRect().left,
         init_top_pos: element.getBoundingClientRect().top,
+        offsetX: element.getBoundingClientRect().width/2,
+        offsetY: element.getBoundingClientRect().height/2,
         clicked: false,
-        blocked: false
+        locked: false
     }
 }
