f54ebad2512693885d7d791a472ce91c394c9600
Miguel Vitor Macedo Dantas <miguelvitordantas@gmail.com>
2022-07-20 21:01:46 +0200

    errors and bugs solved, also code cleaner

--- JavaScript/JogoDaVelha/js/script.js
+++ JavaScript/JogoDaVelha/js/script.js
@@ -12,7 +12,6 @@ const imgs_o = document.querySelectorAll(".o");
 // Creating a list of elements of images X and O
 let list_x_elements = create_list_of_elements(imgs_x);
 let list_o_elements = create_list_of_elements(imgs_o);
-
 function create_list_of_elements(list_of_elements){
     let lista = [];
     for(let element of list_of_elements){
@@ -20,6 +19,7 @@ function create_list_of_elements(list_of_elements){
     }return lista;
 }
 
+// Makin the object that we will work with
 function make_mira_element(element){
     return {
         element : element,
@@ -40,46 +40,47 @@ btn.onclick = () => {
 function set_initial_positions(...list_of_element){
     // console.log(list_of_element);
     for(list of list_of_element){
-        list.forEach((element) => {
-            console.log(element);
-            element.element.style.top = `${element.init_top_pos}px`;
-            element.element.style.left = `${element.init_left_pos}px`;
+        list.forEach((object) => {
+            console.log(object);
+            object.element.style.top = `${element.init_top_pos}px`;
+            object.element.style.left = `${element.init_left_pos}px`;
         });
     }      
 }
 
 // Attributing a function for each img element when clicked
-list_x_elements.forEach((element) => {
-    element.element.onclick = () => {
-        verify_second_click(element);
+list_x_elements.forEach((object) => {
+    object.element.onclick = () => {
+        verify_second_click(object);
     }
 });
 
 // Attributing a function for each img element when clicked
-list_o_elements.forEach((element) => {
-    element.element.onclick = () => {
-        verify_second_click(element);
+list_o_elements.forEach((object) => {
+    object.element.onclick = () => {
+        verify_second_click(object);
     }
 });
 
-function verify_second_click(element){
-    if(element.clicked === true){
+function verify_second_click(object){
+    if(object.clicked === true){
         // second click
-        element.clicked = false;
-        set_automatic_position(this)
+        object.clicked = false;
+        set_automatic_position(object);
+        
     }else{
         // first click
-        element.clicked = true;
+        object.clicked = true;
     }
 }
 
-function set_automatic_position(element){
-    let position_left = element.getBoundingClientRect().left;
-    let position_top = element.getBoundingClientRect().top;
+function set_automatic_position(object){
+    let position_left = object.element.getBoundingClientRect().left;
+    let position_top = object.element.getBoundingClientRect().top;
     // console.log("left ", position_left);
     // console.log("top ", position_top);
-    element.style.left = `${verify_position_left(position_left)}px`;
-    element.style.top = `${verify_position_top(position_top)}px`;
+    object.element.style.left = `${verify_position_left(position_left)}px`;
+    object.element.style.top = `${verify_position_top(position_top)}px`;
 }
 
 function verify_position_left(position_left){
@@ -112,25 +113,24 @@ function verify_position_top(position_top){
 
 // Ading the mousemove action to the page
 document.addEventListener("mousemove", function(event){
-        // console.log(event.clientX, event.clientY);
-        list_x_elements.forEach((element) => {
-            if(element.clicked === true){
-                element = update_position(event, element);
-                element.locked = true;
+        list_x_elements.forEach((object) => {
+            if(object.clicked === true){
+                update_position(event, object);
+                object.locked = true;
+            }else{
+                
             }
         });
 
-        list_o_elements.forEach((element) => {
-            if(element.clicked === true){
-                element = update_position(event, element);
-                element.locked = true;
+        list_o_elements.forEach((object) => {
+            if(object.clicked === true){
+                update_position(event, object);
+                object.locked = true;
             }
         });
     });
 
-function update_position(event, element){
-    // console.log(element);
-    element.style.top = event.clientY - element.offsetY + "px";
-    element.style.left = event.clientX - element.offsetX + "px";
-    return element;
+function update_position(event, object){
+    object.element.style.top = event.clientY - object.offsetY + "px";
+    object.element.style.left = event.clientX - object.offsetX + "px";
     }
